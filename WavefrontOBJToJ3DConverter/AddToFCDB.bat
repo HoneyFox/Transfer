bcc689832254ab24b988d311812cbb9f069bea15
HoneyFox <HoneyFox@users.noreply.github.com>
2023-12-23 22:26:37 +0800

    Fix path issue in bat.

--- WavefrontOBJToJ3DConverter/AddToFCDB.bat
+++ WavefrontOBJToJ3DConverter/AddToFCDB.bat
@@ -1,11 +1,11 @@
-@echo off
+@echo on
 
 G:
 cd "G:\Fleet Command\Graphics"
 
 for %%a in (%*) do (
   copy "%%a" "G:\Fleet Command\Graphics"
-  "G:\Fleet Command\Graphics\cmpUtil.exe" 3d "%%a"
+  "cmpUtil.exe" 3d "%%~nxa"
 )
 
 pause
